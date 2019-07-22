464eef8b8c108e0267285fc9816d3bd2b736fe11
Hacker Brief <hackerbrief@gmail.com>
2019-07-22 19:37:59 -0400

    inject api plugins to app context

---
+++ plugins/api.js
@@ -0,0 +1,14 @@
+export default ({ $axios }, inject) => {
+  inject('fetchConfig', () => {
+    return $axios.$get(process.env.apiUrl + process.env.configUrl)
+  })
+  inject('fetchPosts', () => {
+    return $axios.$get(process.env.apiUrl + process.env.postsUrl)
+  })
+  inject('fetchTools', () => {
+    return $axios.$get(process.env.apiUrl + process.env.toolsUrl)
+  })
+  inject('fetchArticle', id => {
+    return $axios.$get(process.env.apiUrl + process.env.articleUrl + `${id}.json`)
+  })
+}
